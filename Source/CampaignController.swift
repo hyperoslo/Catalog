9895af65cacee3c1cd5392fac1b5e87d5132b883
Vadym Markov <markov.vadym@gmail.com>
2015-06-11 10:43:41 +0200

    Add configuration

--- Source/CampaignController.swift
+++ Source/CampaignController.swift
@@ -3,4 +3,18 @@ import Wall
 
 public class CampaignController: WallController {
 
+  public var campaign: Campaign?
+
+  convenience init(campaign: Campaign) {
+    self.init()
+    self.campaign = campaign
+  }
+
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+
+    Config.Wall.Post.Header.enabled = false
+    Config.Wall.Post.Footer.enabled = false
+    Config.Wall.Post.ActionBar.enabled = false
+  }
 }
