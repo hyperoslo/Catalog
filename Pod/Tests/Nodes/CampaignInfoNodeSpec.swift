a4d9eacb76635eb2befbddbed80413b9e4838726
Vadym Markov <markov.vadym@gmail.com>
2015-06-12 10:58:16 +0200

    Add info node specs

---
+++ Pod/Tests/Nodes/CampaignInfoNodeSpec.swift
@@ -0,0 +1,13 @@
+import Quick
+import Nimble
+import Faker
+import Wall
+
+class CampaignInfoNodeSpec: QuickSpec {
+
+  override func spec() {
+    describe("CampaignInfoNode") {
+      
+    }
+  }
+}
