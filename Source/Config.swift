9b39d71ea6cd77f27e14a57cabfd4c02de834457
Richard Topchii <richard@hyper.no>
2015-06-22 15:25:23 +0200

    Add config for Switch

--- Source/Config.swift
+++ Source/Config.swift
@@ -78,6 +78,7 @@ public class Config: Wall.Config {
       public var verticalPadding: CGFloat = 20
       public var text = Text()
       public var divider = Divider()
+      public var switchControl = SwitchControl()
 
       public struct Text {
         public var string = ""
@@ -97,6 +98,19 @@ public class Config: Wall.Config {
         public var enabled = true
         public var height: CGFloat = 1
         public var backgroundColor = UIColor.lightGrayColor()
+      }
+      
+      public struct SwitchControl {
+        public var enabled = true
+        public var reuseIdentifier = "footer"
+        public var text = "KAMPANJEKLAR"
+        public var font = UIFont.systemFontOfSize(14.0)
+        public var frame = CGRect(x: 0, y: 0, width: 247, height: 50)
+        public var onTextColor = UIColor(red: 52/255.0, green: 224/255.0, blue: 183/255.0, alpha: 1)
+        public var offTextColor = UIColor(red: 176/255.0, green: 182/255.0, blue: 193/255.0, alpha: 1)
+        public var onTintColor = UIColor(red: 52/255.0, green: 224/255.0, blue: 183/255.0, alpha: 1)
+        public var knobMargin: CGFloat = 5.0
+        public var backgroundBorder: CGFloat = 1.0
       }
     }
 
