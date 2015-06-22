3b36f488cf5e788ba75d2d751f510ce2c10b742a
Richard Topchii <richard@hyper.no>
2015-06-22 15:25:38 +0200

    Add datasource binding switch to config

--- Source/CatalogDataSource.swift
+++ Source/CatalogDataSource.swift
@@ -1,5 +1,6 @@
 import Wall
 import AsyncDisplayKit
+import SevenSwitch
 
 public class CatalogDataSource: WallDataSource {
 
@@ -37,4 +38,31 @@ extension CatalogDataSource: ASCollectionViewDataSource {
 
     return cellNode
   }
+}
+extension CatalogDataSource: UICollectionViewDataSource {
+  public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    return UICollectionViewCell()
+  }
+  
+  public func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+    let switchConfig = config!.catalog.footer.switchControl
+    let cell: UICollectionReusableView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionFooter, withReuseIdentifier: switchConfig.reuseIdentifier, forIndexPath: indexPath) as! UICollectionReusableView
+    
+    let campaignReadySwitch = SevenSwitch(frame: switchConfig.frame)
+    campaignReadySwitch.textLabel.font = switchConfig.font
+    campaignReadySwitch.knobText = switchConfig.text
+    campaignReadySwitch.onTextColor = switchConfig.onTextColor
+    campaignReadySwitch.offTextColor = switchConfig.offTextColor
+    campaignReadySwitch.onTintColor = switchConfig.onTintColor
+    campaignReadySwitch.knobMargin = switchConfig.knobMargin
+    campaignReadySwitch.backgroundView.layer.borderWidth = switchConfig.backgroundBorder
+    campaignReadySwitch.layoutSubviews()
+    
+    let center = cell.center
+    campaignReadySwitch.center = center
+    cell.addSubview(campaignReadySwitch)
+    
+    return cell as UICollectionReusableView
+    //    }
+}
 }
